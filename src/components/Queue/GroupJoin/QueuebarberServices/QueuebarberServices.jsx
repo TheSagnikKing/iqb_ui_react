14f0193cb4b5da15f709b91efe0ba6628dea6666
TheSagnikKing <sagniknandy27@gmail.com>
2023-12-07 14:25:11 +0530

    commit

--- src/components/Queue/GroupJoin/QueuebarberServices/QueuebarberServices.jsx
+++ src/components/Queue/GroupJoin/QueuebarberServices/QueuebarberServices.jsx
@@ -22,103 +22,99 @@ const QueuebarberServices = () => {
 
   const [selectServices, setSelectServices] = useState([]);
 
-// For groupJoin
-const groupJoinQueueHandler = (serviceId, serviceName, serviceEWT) => {
+  // For groupJoin
+  const groupJoinQueueHandler = (serviceId, serviceName, serviceEWT) => {
 
-  const serviceData = { serviceId, serviceName, serviceEWT };
+    const serviceData = { serviceId, serviceName, serviceEWT };
 
-  // Check if the service already exists in the array
-  const serviceExists = selectServices.find(
-    (service) => service.serviceId === serviceId
-  );
-  
-  if (!serviceExists) {
-    // Use the spread operator correctly to update the state
-    setSelectServices([...selectServices,serviceData]);
-  }
-  // console.log(barberData);
-};
+    // Check if the service already exists in the array
+    const serviceExists = selectServices.find(
+      (service) => service.serviceId === serviceId
+    );
 
-const barberData = {
-  name: "Aniket Dey",
-  userName: "Aniket",
-  joinedQType: "Group-Join",
-  methodUsed: "Walk-In",
-  barberName: barbername,
-  barberId: barberid,
-  services:selectServices
-};
-
-useEffect(() => {
-  if(selectServices.length > 0){
-    // dispatch({
-    //   type:GROUP_BARBER_SERVICE_JOIN_SUCCESS,
-    //   payload:barberData
-    // })
-
-    const serializedData = JSON.stringify(barberData);
-
-    const storageKey = `barber_${barberid}`;
-
-    // Store the serialized data in local storage
-    localStorage.setItem(storageKey, serializedData);
-  }
-},[selectServices])
-
-// Function to retrieve all barbers from local storage
-const getAllBarbers = () => {
-  const barbers = [];
-
-  // Iterate through local storage keys
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-
-    // Check if the key starts with "barber_" to identify barber data
-    if (key && key.startsWith("barber_")) {
-      const serializedData = localStorage.getItem(key);
-
-      // Parse the serialized data to get the barber object
-      const barberData = JSON.parse(serializedData);
-
-      // Add the barber object to the array
-      barbers.push(barberData);
+    if (!serviceExists) {
+      // Use the spread operator correctly to update the state
+      setSelectServices([...selectServices, serviceData]);
     }
-  }
+    // console.log(barberData);
+  };
 
-  return barbers;
-};
+  const barberData = {
+    name: "Aniket Dey",
+    userName: "Aniket",
+    joinedQType: "Group-Join",
+    methodUsed: "Walk-In",
+    barberName: barbername,
+    barberId: barberid,
+    services: selectServices
+  };
 
-// Example of how to use the getAllBarbers function
-const allBarbers = getAllBarbers();
-console.log(allBarbers);
+  useEffect(() => {
+    if (selectServices.length > 0) {
+
+      const serializedData = JSON.stringify(barberData);
+
+      const storageKey = `barber_${barberid}`;
+
+      // Store the serialized data in local storage
+      localStorage.setItem(storageKey, serializedData);
+
+    }
+
+    // Function to retrieve all barbers from local storage
+    const getAllBarbers = () => {
+      const barbers = [];
+
+      // Iterate through local storage keys
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+
+        // Check if the key starts with "barber_" to identify barber data
+        if (key && key.startsWith("barber_")) {
+          const serializedData = localStorage.getItem(key);
+
+          // Parse the serialized data to get the barber object
+          const barberData = JSON.parse(serializedData);
+
+          // Add the barber object to the array
+          barbers.push(barberData);
+        }
+      }
+
+      return barbers;
+    };
+
+    const allBarbers = getAllBarbers();
+    console.log(allBarbers);
+  }, [selectServices])
 
 
   return (
     <>
 
-          <AdminLayout />
-          <div className="quebarberserv-wrapper">
-            <p>Barber Services</p>
+      <AdminLayout />
+      <div className="quebarberserv-wrapper">
+        <p>Barber Services</p>
 
-            <div>
-              <p>Service Id</p>
-              <p>Service Name</p>
-              <p>Service EWT</p>
-              <p>Action</p>
+        <div>
+          <p>Service Id</p>
+          <p>Service Name</p>
+          <p>Service EWT</p>
+          <p>Action</p>
+        </div>
+
+        {
+          getBarberServicesBybarberId?.response?.map((b) => (
+            <div className='quebarberserv-content' key={b._id}>
+              <p>{b.serviceId}</p>
+              <p>{b.serviceName}</p>
+              <p>{b.serviceEWT}</p>
+              <button onClick={() => groupJoinQueueHandler(b.serviceId, b.serviceName, b.serviceEWT)}>Add Service</button>
             </div>
+          ))
+        }
 
-            {
-              getBarberServicesBybarberId?.response?.map((b) => (
-                <div className='quebarberserv-content' key={b._id}>
-                  <p>{b.serviceId}</p>
-                  <p>{b.serviceName}</p>
-                  <p>{b.serviceEWT}</p>
-                  <button onClick={() => groupJoinQueueHandler(b.serviceId,b.serviceName,b.serviceEWT)}>Add Service</button>
-                </div>
-              ))
-            }
-
-          </div>   
+      </div>
     </>
   )
 }
