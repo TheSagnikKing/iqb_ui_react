69e42e76d3d7d09c899f768eeffab6ef974f363e
TheSagnikKing <sagniknandy27@gmail.com>
2024-01-02 11:47:15 +0530

    commit3

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,31 +1,4 @@
-// // MapComponent.jsx
-// import React from 'react';
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-// const Map = () => {
-//   const mapStyles = {
-//     height: '400px',
-//     width: '100%',
-//   };
-
-//   const location = { lat: 37.7749, lng: -122.4194 };
-
-//   return (
-//     <LoadScript
-//       googleMapsApiKey="AIzaSyC3nMS-ORtw4HrAQA-6fNnQUrSf-dnytYk"
-//     >
-//       <GoogleMap
-//         mapContainerStyle={mapStyles}
-//         zoom={10}
-//         center={location}
-//       >
-//         <Marker position={location} />
-//       </GoogleMap>
-//     </LoadScript>
-//   );
-// };
-
-// export default Map;
 
 import React from 'react'
 
