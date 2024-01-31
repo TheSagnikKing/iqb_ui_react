a4122763909492f45043fa5bf1ca953d0090fa78
TheSagnikKing <sagniknandy27@gmail.com>
2024-01-31 16:37:58 +0530

    commit

--- src/components/layout/Admin/Adminheader/AdminMenu.jsx
+++ src/components/layout/Admin/Adminheader/AdminMenu.jsx
@@ -6,12 +6,10 @@ import { Link } from 'react-router-dom'
 
 const AdminMenu = ({ menu_logo, menu_title, category, menu_link }) => {
 
-    const [dropdown, setDropdown] = useState(false)
-
     return (
         <Link to={`${menu_link}`} style={{color:"#000", textDecoration:"none"}}>
             <div className="nav1-menu"
-                onClick={(e) => setDropdown(!dropdown)}
+                
             >
                 <div >
                     <div>
--- src/components/layout/header/Menu2.jsx
+++ src/components/layout/header/Menu2.jsx
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom'
 
 const Menu2 = ({ menu_logo, menu_title, category, menu_link }) => {
 
-    const [dropdown, setDropdown] = useState(false)
+
 
     return (
             <Link to={`${menu_link}`} style={{color:"#000", textDecoration:"none"}}>
             <div className="nav1-menu"
-                onClick={(e) => setDropdown(!dropdown)}
+               
             >
                 <div >
                     <div>
